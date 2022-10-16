18d9ac95b198854fd9f7f52a5f295cecf1bde20b
vidinararia <vidinararia@gmail.com>
2022-10-16 20:52:40 +0800

    add React pagination

--- src/component/table/Table.js
+++ src/component/table/Table.js
@@ -1,5 +1,8 @@
 import { useMutation } from "@apollo/client";
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import ReactPaginate from "react-paginate";
 import { useNavigate } from "react-router-dom";
 import { DELETE_DATA_PROJECT_BY_ID } from "../../graphql/mutation/Mutation";
 import { GET_DATA_PROJECT } from "../../graphql/query/Query";
@@ -14,6 +17,21 @@ export const Table = ({
   let navigate = useNavigate();
 
   const IDRConvert = Intl.NumberFormat("id-ID");
+  const [currenItems, setCurrentItems] = useState([]);
+  const [pageCount, setPageCount] = useState(0);
+  const [itemOffset, setItemOffset] = useState(0);
+  const itemsPerPage = 5;
+
+  useEffect(() => {
+    const endOffset = itemOffset + itemsPerPage;
+    setCurrentItems(data?.project.slice(itemOffset, endOffset));
+    setPageCount(Math.ceil(data?.project.length / itemsPerPage));
+  }, [itemOffset, itemsPerPage, data?.project]);
+
+  const handlePageClick = (event) => {
+    const newOffset = (event.selected * itemsPerPage) % data?.project.length;
+    setItemOffset(newOffset);
+  };
 
   const [deleteProject, { loading }] = useMutation(DELETE_DATA_PROJECT_BY_ID, {
     refetchQueries: [GET_DATA_PROJECT],
@@ -24,8 +42,8 @@ export const Table = ({
   }
 
   return (
-    <div className="overflow-auto shadow">
-      <table className="w-full">
+    <div className="overflow-auto">
+      <table className="w-full shadow">
         <thead>
           <tr className="border border-gray-300 bg-gray-50">
             {column.map((col) => (
@@ -37,7 +55,7 @@ export const Table = ({
           </tr>
         </thead>
         <tbody>
-          {data?.project.map((col) => (
+          {currenItems.map((col) => (
             <tr key={col.id} className="bg-white border border-gray-300 ">
               <td className="p-3 text-sm whitespace-nowrap font-medium text-gray-700">
                 {col.name}
@@ -89,6 +107,30 @@ export const Table = ({
           ))}
         </tbody>
       </table>
+      <ReactPaginate
+        breakLabel="..."
+        nextLabel={"next >"}
+        previousLabel={"< previous"}
+        renderOnZeroPageCount={null}
+        pageRangeDisplayed={3}
+        pageCount={pageCount}
+        onPageChange={handlePageClick}
+        containerClassName={
+          "flex justify-end items-center list-none gap-[5px] pt-3 text-center "
+        }
+        pageClassName={
+          "font-semibold rounded-full w-[30px] border hover:border-green-500 p-[4px] cursor-pointer text-sm"
+        }
+        previousLinkClassName={
+          "font-semibold cursor-pointer p-[4px] hover:text-green-500"
+        }
+        nextLinkClassName={
+          "font-semibold cursor-pointer p-[4px] hover:text-green-500"
+        }
+        activeClassName={
+          "rounded border border-green-500 bg-green-500 hover:border-green-600 hover:bg-green-600 text-white p-[4px]"
+        }
+      />
     </div>
   );
 };
