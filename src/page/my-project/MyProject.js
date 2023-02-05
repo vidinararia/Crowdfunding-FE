9f1b7823e273855faccebc766f6f9669b6562e7e
vidinararia <vidinararia@gmail.com>
2023-02-05 15:35:39 +0800

    add axios post

--- src/page/my-project/MyProject.js
+++ src/page/my-project/MyProject.js
@@ -1,23 +1,18 @@
 import React, { useState, useEffect } from "react";
 import { Table } from "../../component/table/Table";
 import Navbar from "../../component/navbar/Navbar";
-import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { baseApi } from "../../utils/api/Api";
+import { useNavigate } from "react-router-dom";
 
 export default function MyProject() {
+  const [data, setData] = useState([]);
+
   let navigate = useNavigate();
 
-  const [data, setData] = useState([]);
-  // const [data, setData] = useState([
-  //   {
-  //     name: "project1",
-  //     target: "100000",
-  //     amount: "0",
-  //     startdate: "2022-10-02",
-  //     dateline: "2022-10-02",
-  //   },
-  // ]);
+  const handlePageCreate = () => {
+    navigate("/create-project");
+  };
 
   useEffect(() => {
     axios
@@ -41,7 +36,13 @@ export default function MyProject() {
       <Navbar />
       <div className="bg-gray-200 p-5 h-screen md:px-16">
         <div className="mt-16">
-          <h1 className="text-xl font-bold mb-3">My Project</h1>
+          <h1 className="text-xl font-bold mb-3">All Funding</h1>
+          <button
+            className="bg-green-500 rounded hover:bg-green-600 text-white font-semibold md:py-2 md:px-6 px-4 py-2 mb-3 transition-all"
+            onClick={handlePageCreate}
+          >
+            Create Project
+          </button>
           <Table column={column} data={data} deleteHidden donateHidden />
         </div>
       </div>
